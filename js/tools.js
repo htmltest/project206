333854c298ce231c8c9e50b5ff95785c94c7278a
htmltest <struev.vs@gmail.com>
2022-09-06 15:10:50 +0300

    update
    правки по проверке полей, сообщениям о результатах отправки и анимации кнопок

--- js/tools.js
+++ js/tools.js
@@ -1,5 +1,33 @@
 $(document).ready(function() {
 
+    $.validator.addMethod('imei',
+        function(curValue, element) {
+            return this.optional(element) || curValue.match(/^\d{15}$/);
+        },
+        'IMEI должен состоять из 15 цифр'
+    );
+
+    $.validator.addMethod('serialPad',
+        function(curValue, element) {
+            return this.optional(element) || curValue.match(/^\d{15}$/);
+        },
+        'Серийный номер должен состоять из 15 цифр'
+    );
+
+    $.validator.addMethod('serialTV',
+        function(curValue, element) {
+            return this.optional(element) || curValue.match(/^RU\w\w\w\w\w\w\w\w\w\w\w\w\w\w\w\w\w\w$/i);
+        },
+        'Серийный номер должен состоять из 20 латинских букв и цифр'
+    );
+
+    $.validator.addMethod('serialBook',
+        function(curValue, element) {
+            return this.optional(element) || curValue.match(/^\d{18}$/);
+        },
+        'Серийный номер должен состоять из 18 цифр'
+    );
+
     $('body').on('focus', '.form-input input', function() {
         $(this).parent().addClass('focus');
     });
@@ -42,124 +70,7 @@ $(document).ready(function() {
 
 	$('input.imei').attr('autocomplete', 'off');
 
-    $('body').on('keyup', '.form-input input.imei', function(e) {
-        var curInput = $(this);
-        if ((e.keyCode >= 48 && e.keyCode <= 57) || (e.keyCode >= 96 && e.keyCode <= 105)) {
-            var curValue = curInput.val();
-            if (curValue.length == 15) {
-                curInput.parent().find('label').remove();
-                curInput.removeClass('error').addClass('valid');
-            } else {
-                curInput.parent().find('label').remove();
-                curInput.removeClass('valid error');
-                curInput.after('<label class="error">IMEI должен состоять из 15 символов</label>');
-            }
-        } else {
-            curInput.parent().find('label').remove();
-            curInput.addClass('error').removeClass('valid');
-            curInput.after('<label class="error">IMEI может содержать только цифры от 0 до 9</label>');
-        }
-	});
-
-	$('body').on('focus', '.form-input input.imei', function(e) {
-        var curInput = $(this);
-        curInput.parent().find('label').remove();
-        var curValue = curInput.val();
-        if (curValue.length != 15) {
-            curInput.after('<label class="error">IMEI должен состоять из 15 символов</label>');
-        }
- 	});
-
 	$('input.serial').attr('autocomplete', 'off');
-
-    $('body').on('keyup', '.form-input input.serial', function(e) {
-        var curInput = $(this);
-        var curSelect = $('.model-select');
-        var curValueSelect = curSelect.val();
-        if (curValueSelect != '') {
-            var curOption = curSelect.find('option:selected');
-            var curType = curOption.attr('data-type');
-            curInput.parent().find('label').remove();
-            if (curType == 'pad') {
-                if ((e.keyCode >= 48 && e.keyCode <= 57) || (e.keyCode >= 96 && e.keyCode <= 105)) {
-                    var curValue = curInput.val();
-                    if (curValue.length == 15) {
-                        curInput.parent().find('label').remove();
-                        curInput.removeClass('error').addClass('valid');
-                    } else {
-                        curInput.parent().find('label').remove();
-                        curInput.removeClass('valid error');
-                        curInput.after('<label class="error">Серийный номер должен состоять из 15 символов</label>');
-                    }
-                } else {
-                    curInput.parent().find('label').remove();
-                    curInput.addClass('error').removeClass('valid');
-                    curInput.after('<label class="error">Серийный номер может содержать только цифры от 0 до 9</label>');
-                }
-            } else if (curType == 'tv') {
-                console.log(e.keyCode);
-                if ((e.keyCode >= 48 && e.keyCode <= 57) || (e.keyCode >= 96 && e.keyCode <= 105) || (e.keyCode >= 65 && e.keyCode <= 90) || e.keyCode == 16) {
-                    var curValue = curInput.val();
-                    if (curValue.length == 20) {
-                        curInput.parent().find('label').remove();
-                        curInput.removeClass('error').addClass('valid');
-                    } else {
-                        curInput.parent().find('label').remove();
-                        curInput.removeClass('valid error');
-                        curInput.after('<label class="error">Серийный номер должен состоять из 20 символов</label>');
-                    }
-                } else {
-                    curInput.parent().find('label').remove();
-                    curInput.addClass('error').removeClass('valid');
-                    curInput.after('<label class="error">Серийный номер может содержать только цифры и латинские буквы</label>');
-                }
-            } else {
-                if ((e.keyCode >= 48 && e.keyCode <= 57) || (e.keyCode >= 96 && e.keyCode <= 105)) {
-                    var curValue = curInput.val();
-                    if (curValue.length == 18) {
-                        curInput.parent().find('label').remove();
-                        curInput.removeClass('error').addClass('valid');
-                    } else {
-                        curInput.parent().find('label').remove();
-                        curInput.removeClass('valid error');
-                        curInput.after('<label class="error">Серийный номер должен состоять из 18 символов</label>');
-                    }
-                } else {
-                    curInput.parent().find('label').remove();
-                    curInput.addClass('error').removeClass('valid');
-                    curInput.after('<label class="error">Серийный номер может содержать только цифры от 0 до 9</label>');
-                }
-            }
-        }
-	});
-
-	$('body').on('focus', '.form-input input.serial', function(e) {
-        var curInput = $(this);
-        var curSelect = $('.model-select');
-        var curValueSelect = curSelect.val();
-        if (curValueSelect != '') {
-            var curOption = curSelect.find('option:selected');
-            var curType = curOption.attr('data-type');
-            curInput.parent().find('label').remove();
-            if (curType == 'pad') {
-                var curValue = curInput.val();
-                if (curValue.length != 15) {
-                    curInput.after('<label class="error">Серийный номер должен состоять из 15 символов</label>');
-                }
-            } else if (curType == 'tv') {
-                var curValue = curInput.val();
-                if (curValue.length != 20) {
-                    curInput.val('RU');
-                    curInput.after('<label class="error">Серийный номер должен состоять из 20 символов</label>');
-                }
-            } else {
-                var curValue = curInput.val();
-                if (curValue.length != 18) {
-                    curInput.after('<label class="error">Серийный номер должен состоять из 18 символов</label>');
-                }
-            }
-        }
-	});
 
     $('.model-select').change(function() {
         var curSelect = $(this);
@@ -168,9 +79,11 @@ $(document).ready(function() {
             var curOption = curSelect.find('option:selected');
             var curType = curOption.attr('data-type');
             $('input.serial').val('').trigger('blur');
+            $('input.serial').removeClass('serialPad serialTV serialBook');
             $('input.serial').parent().find('label').remove();
             if (curType == 'pad') {
                 $('input.serial').mask('000000000000000');
+                $('input.serial').addClass('serialPad');
             } else if (curType == 'tv') {
                 $('input.serial').mask('RUZZZZZZZZZZZZZZZZZZ', {
                     translation: {
@@ -179,8 +92,10 @@ $(document).ready(function() {
                         }
                     }
                 });
+                $('input.serial').addClass('serialTV');
             } else {
                 $('input.serial').mask('000000000000000000');
+                $('input.serial').addClass('serialBook');
             }
         }
     });
@@ -246,86 +161,37 @@ function initForm(curForm) {
     curForm.validate({
         ignore: '',
         submitHandler: function(form) {
-            var isError = false;
-            curForm.find('.form-input input.imei').each(function(e) {
-                var curInput = $(this);
-                var curValue = curInput.val();
-                if (curValue.length != 15) {
-                    isError = true;
-                    curInput.parent().find('label').remove();
-                    curInput.removeClass('valid').addClass('error');
-                    curInput.after('<label class="error">IMEI должен состоять из 15 символов</label>');
-                }
-            });
-            curForm.find('.form-input input.serial').each(function(e) {
-                var curInput = $(this);
-                var curSelect = $('.model-select');
-                var curValueSelect = curSelect.val();
-                if (curValueSelect != '') {
-                    var curOption = curSelect.find('option:selected');
-                    var curType = curOption.attr('data-type');
-                    curInput.parent().find('label').remove();
-                    if (curType == 'pad') {
-                        var curValue = curInput.val();
-                        if (curValue.length != 15) {
-                            isError = true;
-                            curInput.parent().find('label').remove();
-                            curInput.removeClass('valid').addClass('error');
-                            curInput.after('<label class="error">Серийный номер должен состоять из 15 символов</label>');
-                        }
-                    } else if (curType == 'tv') {
-                        var curValue = curInput.val();
-                        if (curValue.length != 20) {
-                            isError = true;
-                            curInput.parent().find('label').remove();
-                            curInput.removeClass('valid').addClass('error');
-                            curInput.after('<label class="error">Серийный номер должен состоять из 20 символов</label>');
-                        }
-                    } else {
-                        var curValue = curInput.val();
-                        if (curValue.length != 18) {
-                            isError = true;
-                            curInput.parent().find('label').remove();
-                            curInput.removeClass('valid').addClass('error');
-                            curInput.after('<label class="error">Серийный номер должен состоять из 18 символов</label>');
-                        }
-                    }
-                }
-            });
-            if (!isError) {
-                curForm.find('.form-footer-error').removeClass('visible');
-                curForm.find('.form-footer-success').removeClass('visible');
-                curForm.addClass('loading');
-                var curData = curForm.serialize();
-                $.ajax({
-                    type: 'POST',
-                    url: curForm.attr('action'),
-                    dataType: 'json',
-                    data: curData,
-                    cache: false
-                }).fail(function(jqXHR, textStatus, errorThrown) {
-                    curForm.find('.form-footer-error').addClass('visible').find('.message-text').html(textStatus);
-                    curForm.find('.form-footer-link').addClass('hidden');
-                    curForm.removeClass('loading');
-                }).done(function(data) {
+            curForm.find('.form-footer-error').removeClass('visible');
+            curForm.find('.form-footer-success').removeClass('visible');
+            curForm.addClass('loading');
+            var curData = curForm.serialize();
+            $.ajax({
+                type: 'POST',
+                url: curForm.attr('action'),
+                dataType: 'json',
+                data: curData,
+                cache: false
+            }).fail(function(jqXHR, textStatus, errorThrown) {
+                curForm.find('.form-footer-error').addClass('visible').find('.message-text').html(textStatus);
+                curForm.removeClass('loading');
+                $('html, body').animate({'scrollTop': $('.main-form-title').offset().top});
+            }).done(function(data) {
+                curForm.find('label.error').remove();
+                if (data.status) {
+                    curForm.find('.form-footer-success').addClass('visible');
+                    curForm.find('.form-input input').val('').trigger('blur');
+                    curForm.find('.form-input input').removeClass('error valid');
                     curForm.find('label.error').remove();
-                    if (data.status) {
-                        curForm.find('.form-footer-success').addClass('visible');
-                        curForm.find('.form-footer-link').addClass('hidden');
-                        curForm.find('.form-input input').val('').trigger('blur');
-                        curForm.find('.form-input input').removeClass('error valid');
-                        curForm.find('label.error').remove();
-                        curForm.find('.form-checkbox input').prop('checked', false);
-                        curForm.find('.form-select select').val(null).trigger('change');
-                        curForm.find('.select2-container').removeClass('select2-container--full select2-container--full-multiple');
-                        curForm.find('.form-select select').removeClass('valid');
-                    } else {
-                        curForm.find('.form-footer-link').addClass('hidden');
-                        curForm.find('.form-footer-error').addClass('visible').find('.message-text').html(data.message);
-                    }
-                    curForm.removeClass('loading');
-                });
-            }
+                    curForm.find('.form-checkbox input').prop('checked', false);
+                    curForm.find('.form-select select').val(null).trigger('change');
+                    curForm.find('.select2-container').removeClass('select2-container--full select2-container--full-multiple');
+                    curForm.find('.form-select select').removeClass('valid');
+                } else {
+                    curForm.find('.form-footer-error').addClass('visible').find('.message-text').html(data.message);
+                }
+                curForm.removeClass('loading');
+                $('html, body').animate({'scrollTop': $('.main-form-title').offset().top});
+            });
         }
     });
 }
